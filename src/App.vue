5ef4a2a9a7ba524ef24faad865e338c3d79a8c97
Miguel Silva <mnsilva2@gmail.com>
2024-05-12 16:21:29 +0000

    Shows Error Message if something fails

--- src/App.vue
+++ src/App.vue
@@ -15,17 +15,27 @@ onMounted(() => {
   <main
     class="h-screen w-screen flex overflow-hidden justify-center bg-neutral-200"
   >
-    <div class="flex max-w-7xl w-full flex-col md:flex-row z-10">
-      <div class="w-full md:w-1/2 p-4 lg:p-8">
-        <div class="text-2xl mb-4 text-white">Sortable Post List</div>
-        <post-list />
-      </div>
+    <template v-if="!store.error">
+      <div class="flex max-w-7xl w-full flex-col md:flex-row z-10">
+        <div class="w-full md:w-1/2 p-4 lg:p-8">
+          <div class="text-2xl mb-4 text-white">Sortable Post List</div>
+          <post-list />
+        </div>
 
-      <div class="max-h-full w-full md:w-1/2 p-4 lg:p-8 overflow-hidden">
-        <history-list class="max-h-full" />
+        <div class="max-h-full w-full md:w-1/2 p-4 lg:p-8 overflow-hidden">
+          <history-list class="max-h-full" />
+        </div>
       </div>
+      <div
+        class="absolute w-full h-full bg-purple-800 triangle-clip-path"
+      ></div>
+    </template>
+    <div v-else class="h-full pt-20 flex flex-col gap-2">
+      <span class="text-2xl"
+        >Oh no! Something went wrong while loading the application!</span
+      >
+      <pre class="text-sm">{{ store.error }}</pre>
     </div>
-    <div class="absolute w-full h-full bg-purple-800 triangle-clip-path"></div>
   </main>
 </template>
 
